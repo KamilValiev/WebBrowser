7f3985ce7e911a2958ee4d2d874c8995f4a2bccf
Kamil <camil.valiev2014@yandex.ru>
2021-03-19 19:16:57 +0300

    Add MyLab

--- MainForm.cs
+++ MainForm.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Security.Permissions;
+
 
 namespace Какой_то_урок
 {
@@ -19,8 +21,26 @@ namespace Какой_то_урок
         {
             InitializeComponent();
         }
-
-        private void toolStripButton1_Click(object sender, EventArgs e)
+        private void Navigate(String address)
+        {
+            if (String.IsNullOrEmpty(address)) return;
+            if (address.Equals("about:blank")) return;
+            if (!address.StartsWith("http://www.") && !address.StartsWith("https://www."))
+            { 
+                 address = "http://www." + address;
+            }
+            try
+            {
+                webBrowser.Navigate(new Uri(address));
+            }
+            catch (System.UriFormatException)
+            {
+                return;
+            }
+        }
+        
+        
+        private void menuStripAddPageBut_Click(object sender, EventArgs e)
         {
             WebBrowser browser = new WebBrowser();
             browser.Visible = true;
@@ -28,23 +48,24 @@ namespace Какой_то_урок
             browser.Dock = DockStyle.Fill;
             browser.DocumentCompleted += browser_DocumentCompleted;
 
-            tabControl1.TabPages.Add("Новая");
-            tabControl1.SelectTab(kolPages);
-            tabControl1.SelectedTab.Controls.Add(browser);
+            tabControl.TabPages.Add("Новая");
+            tabControl.SelectTab(kolPages);
+            tabControl.SelectedTab.Controls.Add(browser);
             kolPages++;
         }
 
         private void toolStripButton2_Click(object sender, EventArgs e)
         {
-            if(!String.IsNullOrEmpty(toolStripText.Text))
+            if(!String.IsNullOrEmpty(menuStripAdressLine.Text))
             {
-                ((WebBrowser)tabControl1.SelectedTab.Controls[0]).Navigate(toolStripText.Text);
+                ((WebBrowser)tabControl.SelectedTab.Controls[0]).Navigate(menuStripAdressLine.Text);
             }
         }
+        
 
         void browser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
         {
-            tabControl1.SelectedTab.Text = ((WebBrowser)tabControl1.SelectedTab.Controls[0]).DocumentTitle;
+            tabControl.SelectedTab.Text = ((WebBrowser)tabControl.SelectedTab.Controls[0]).DocumentTitle;
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -55,10 +76,52 @@ namespace Какой_то_урок
             browser.Dock = DockStyle.Fill;
             browser.DocumentCompleted += browser_DocumentCompleted;
 
-            tabControl1.TabPages.Add("www.Google.com");
-            tabControl1.SelectedTab.Controls.Add(browser);
-            tabControl1.SelectTab(kolPages);
+            tabControl.TabPages.Add("новая");
+            tabControl.SelectedTab.Controls.Add(browser);
+            tabControl.SelectTab(kolPages);
             kolPages++;
+        }
+
+
+        private void webBrowser1_CanGoBackChanged(object sender, EventArgs e)
+        {
+            menuStripGoBackBut.Enabled = webBrowser.CanGoBack;
+        }
+        private void menuStripGoBackBut_Click(object sender, EventArgs e)
+        {
+            webBrowser.GoBack();
+        }
+
+
+        private void menuStripGoForwardBut_Click(object sender, EventArgs e)
+        {
+            webBrowser.GoForward();
+        }
+        private void webBrowser_CanGoForwardChanged(object sender, EventArgs e)
+        {
+            menuStripGoForwardBut.Enabled = webBrowser.CanGoForward;
+        }
+
+
+        private void menuStripAdressLine_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                Navigate(menuStripAdressLine.Text);
+            }
+        }
+
+        private void menuStripReloadBut_Click(object sender, EventArgs e)
+        {
+            if (!webBrowser.Url.Equals("about:blank"))
+            {
+                webBrowser.Refresh();
+            } 
+        }
+
+        private void menuStripSearchBut_Click(object sender, EventArgs e)
+        {
+            Navigate(menuStripAdressLine.Text);
         }
     }
 }
