26457e381b91c51643f3f658d979015cc418c32a
Kamil <camil.valiev2014@yandex.ru>
2021-03-19 15:33:07 +0300

    k

---
+++ MainForm.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace Какой_то_урок
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.toolStrip = new System.Windows.Forms.ToolStrip();
+            this.toolStripAdd = new System.Windows.Forms.ToolStripButton();
+            this.toolStripText = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripNext = new System.Windows.Forms.ToolStripButton();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.toolStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip
+            // 
+            this.toolStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripAdd,
+            this.toolStripText,
+            this.toolStripNext});
+            this.toolStrip.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip.Name = "toolStrip";
+            this.toolStrip.Size = new System.Drawing.Size(800, 27);
+            this.toolStrip.TabIndex = 0;
+            this.toolStrip.Text = "tStrip";
+            // 
+            // toolStripAdd
+            // 
+            this.toolStripAdd.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripAdd.Image = ((System.Drawing.Image)(resources.GetObject("toolStripAdd.Image")));
+            this.toolStripAdd.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripAdd.Name = "toolStripAdd";
+            this.toolStripAdd.Size = new System.Drawing.Size(29, 24);
+            this.toolStripAdd.Text = "+";
+            this.toolStripAdd.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripText
+            // 
+            this.toolStripText.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.toolStripText.Name = "toolStripText";
+            this.toolStripText.Size = new System.Drawing.Size(100, 27);
+            // 
+            // toolStripNext
+            // 
+            this.toolStripNext.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripNext.Image = ((System.Drawing.Image)(resources.GetObject("toolStripNext.Image")));
+            this.toolStripNext.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripNext.Name = "toolStripNext";
+            this.toolStripNext.Size = new System.Drawing.Size(32, 24);
+            this.toolStripNext.Text = "Go";
+            this.toolStripNext.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 27);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(800, 423);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.toolStrip);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip.ResumeLayout(false);
+            this.toolStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip;
+        private System.Windows.Forms.ToolStripButton toolStripAdd;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.ToolStripTextBox toolStripText;
+        private System.Windows.Forms.ToolStripButton toolStripNext;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Какой_то_урок
+{
+    public partial class MainForm : Form
+    {
+        private int kolPages = 0;
+
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            WebBrowser browser = new WebBrowser();
+            browser.Visible = true;
+            browser.ScriptErrorsSuppressed = true;
+            browser.Dock = DockStyle.Fill;
+            browser.DocumentCompleted += browser_DocumentCompleted;
+
+            tabControl1.TabPages.Add("Новая");
+            tabControl1.SelectTab(kolPages);
+            tabControl1.SelectedTab.Controls.Add(browser);
+            kolPages++;
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            if(!String.IsNullOrEmpty(toolStripText.Text))
+            {
+                ((WebBrowser)tabControl1.SelectedTab.Controls[0]).Navigate(toolStripText.Text);
+            }
+        }
+
+        void browser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            tabControl1.SelectedTab.Text = ((WebBrowser)tabControl1.SelectedTab.Controls[0]).DocumentTitle;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        { 
+            WebBrowser browser = new WebBrowser();
+            browser.Visible = true;
+            browser.ScriptErrorsSuppressed = true;
+            browser.Dock = DockStyle.Fill;
+            browser.DocumentCompleted += browser_DocumentCompleted;
+
+            tabControl1.TabPages.Add("www.Google.com");
+            tabControl1.SelectedTab.Controls.Add(browser);
+            tabControl1.SelectTab(kolPages);
+            kolPages++;
+        }
+    }
+}
